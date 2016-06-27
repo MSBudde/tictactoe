8a25efd738b2a8ab31fd8815384b81632017a88a
Michael.s.Budde@gmail.com </ignore>
2016-06-27 11:13:39 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// var gameBoard = [
+//   ['top1','top2','top3']
+//   ['mid1','mid2','mid3']
+//   ['bot1','bot2','bot3']
+// ]
+
+  var t1 = document.getElementById('top1');
+  var  t2 = document.getElementById('top2');
+  var  t3 = document.getElementById('top3');
+  var  m1 = document.getElementById('mid1');
+  var  m2 = document.getElementById('mid2');
+  var  m3 = document.getElementById('mid3');
+  var  b1 = document.getElementById('bot1');
+  var  b2 = document.getElementById('bot2');
+  var  b3 = document.getElementById('bot3');
+
+
+var resetGame = document.getElementById('reset');
+
+document.getElementsByClassName('squaret').addEventListener("click",
+function(){
+  alert("hey this is top1!")
+})
